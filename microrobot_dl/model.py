26e9a9c219f29589e05ae8799e7fd54a55b5352d
chihuang <amochliu@gmail.com>
2025-11-23 00:56:55 +0000

    refactor model.py

--- microrobot_dl/model.py
+++ microrobot_dl/model.py
@@ -1,7 +1,22 @@
-from torchvision.models import resnet18, resnet34, resnet50
+from importlib import import_module
 
+torch_vision = "torchvision.models"
 MODEL_MAP = {
-    "resnet18": resnet18,
-    "resnet34": resnet34,
-    "resnet50": resnet50,
+    "resnet18": torch_vision,
+    "resnet34": torch_vision,
+    "resnet50": torch_vision,
 }
+
+
+def get_model(model_name: str, *arg, **kwg):
+    if model_name not in MODEL_MAP:
+        return None
+
+    module = MODEL_MAP.get(model_name)
+
+    try:
+        mod = import_module(module)
+        cls = getattr(mod, model_name)
+        return cls(*arg, **kwg)
+    except (ImportError, AttributeError):
+        return None
